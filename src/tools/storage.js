8c4f3a4e973eaf33b4c616a79f1f596b240ea5e6
TheExi1eXiao <404251171@qq.com>
2019-05-04 19:12:29 +0800

    storage

--- src/tools/storage.js
+++ src/tools/storage.js
@@ -1,1 +1,40 @@
-storage.js
+
+//封装localStorage对象
+export default class Storage {
+	//设置localStorage方法
+	setLocalStorage(key,data,time){
+		let date = new Date();
+		let value = {
+			value:data,
+			time:time ? data.getTime() + 120000 * time : time
+		}
+		localStorage.setItem(JSON.stringify(value))
+	},
+	//获取localStorage方法
+	getLocalStorage(key){
+		let data = JSON.parse(localStorage.getItem(key));
+		let date = new Date();
+		let now = date.getTime();
+		if(data == null){
+			return undefined
+		}
+		else {
+			if(data.time){
+				if(data.time > now){
+					return data.value
+				}
+				else{
+					this.removeLocalStorage(key);
+					return undefined;
+				}
+			}
+			else{
+				return data.value;
+			}
+		}
+	},
+	//删除localStorage方法
+	removeLocalStorage(key){
+		localStorage.removeItem(key);
+	}
+}
